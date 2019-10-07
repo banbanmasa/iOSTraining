cfb804b3a6eb2293e234368b894a3d6bff4025ff
masanobu.ban <masanobu.ban@studist.jp>
2019-10-07 16:42:17 +0900

    json 1.1

--- Swift/before/day1/1.1/JSON.playground/Contents.swift
+++ Swift/before/day1/1.1/JSON.playground/Contents.swift
@@ -1,3 +1,21 @@
 //: Playground - noun: a place where people can play
 
-let dict: [String : Any] = [:]
+let dict: [String : [[String : Any?]]] = [
+    "users" : [
+        ["name" : "Dr. Emmett Brown",
+         "gender" : 1, "era" : 1985,
+         "age" : 65,
+         "visit" : [1885, 1955, 1985, 2015],],
+        ["name" : "Marty McFly",
+        "gender" : 1,
+        "era" : 1985,
+        "age" : 17,
+        "visit" : [1885, 1955, 1985, 2015],],
+        ["name" : "Lorraine Baines",
+        "gender" : 0,
+        "era" : 1955,
+        "age" : 18,
+        "visit" : nil]
+    ]
+]
+print(dict)
