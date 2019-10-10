7786b7595a5f122ed74f8b1a0f36eea9664aadd9
masanobu.ban <masanobu.ban@studist.jp>
2019-10-10 12:30:26 +0900

    2-1

--- Swift/before/day2/1.1/NavigationSample/NavigationSample/ViewController.swift
+++ Swift/before/day2/1.1/NavigationSample/NavigationSample/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return [.portrait, .landscape]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,6 +28,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("will layout subviews")
+    }
+
+    // [3] 回転前処理
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        if size.width > size.height {
+            print("to Landscape")
+        } else {
+            print("to Portrait")
+        }
+    }
+
+    // [4] traitCollectionが変更される場合、任意のアニメーションをここで設定
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        print("will change collection")
+    }
+
+    // [5] traitCollection変更完了処理
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        print("did change collection")
+    }
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
         
     }
--- Swift/before/day2/2.1/ViewSample/ViewSample/FirstViewController.swift
+++ Swift/before/day2/2.1/ViewSample/ViewSample/FirstViewController.swift
@@ -13,6 +13,43 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var frame: CGRect = .zero
+        frame.origin.x = 10
+        frame.origin.y = 100
+        frame.size.width = 200
+        frame.size.height = 50
+        let view = UIView(frame: frame) // UIViewの初期化
+        view.backgroundColor = .blue // デフォルトでは透明なので配置されてもわからない。そこで適当な色を入れておく
+        self.view.addSubview(view) // viewを追加. self.viewはUIViewControllerのプロパティ
+        var frame2: CGRect = .zero
+        frame2.origin.x = 100
+        frame2.origin.y = 10
+        frame2.size.width = 10
+        frame2.size.height = 10
+        let button = UIButton(frame: frame2)
+        button.backgroundColor = .green
+        self.view.addSubview(button)
+        let label = UILabel(frame: CGRect(x: 20, y: 600, width: 300, height: 21))
+        label.text = "hogehoge aho!"
+        self.view.addSubview(label)
+        let textField = UITextField(frame: CGRect(x: 20, y: 200, width: 100, height: 21))
+        textField.backgroundColor = .white
+        self.view.addSubview(textField)
+        
+        let imageView = UIImageView(frame: CGRect(x: 20, y: 150, width: 128, height: 128))
+        imageView.image = UIImage(named: "ojisan.png")
+        self.view.addSubview(imageView)
+        
+        let imageView2 = UIImageView(frame: CGRect(x: 40, y: 250, width: 128, height: 128))
+        imageView2.image = UIImage(named: "first.png")
+        self.view.addSubview(imageView2)
+        
+        for view in view.subviews {
+            if view is UIImageView {
+                view.removeFromSuperview()
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
