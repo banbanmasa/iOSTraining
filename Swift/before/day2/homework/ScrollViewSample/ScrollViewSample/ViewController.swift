999bfc1d6c1da4bf6a061b252d0afab89352d5de
masanobu.ban <masanobu.ban@studist.jp>
2019-10-11 15:09:44 +0900

    day2finish

--- Swift/before/day2/homework/ScrollViewSample/ScrollViewSample/ViewController.swift
+++ Swift/before/day2/homework/ScrollViewSample/ScrollViewSample/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private struct Const {
+        static let movedFrame = CGRect(x: 387, y: 338, width: 170, height: 170)
+        static let cgPoint = CGPoint(x: 387, y:0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,7 @@ class ViewController: UIViewController {
         
         let image = UIImage(named:"big_image")!
         let scale = UIScreen.main.scale
-        let size = CGSize(width: image.size.width * scale, height: image.size.height * scale)
+        let size = CGSize(width: image.size.width * scale, height: image.size.height * scale * 2)
         let imageView = UIImageView(frame:CGRect(origin: .zero, size: size))
     
         imageView.image = image
@@ -29,8 +33,15 @@ class ViewController: UIViewController {
         scrollView.maximumZoomScale = 3
         scrollView.minimumZoomScale = 0.5
         scrollView.delegate = self
-    }
+        scrollView.scrollsToTop = true
+        UIView.animate(withDuration: 10) {
+            scrollView.setContentOffset(Const.cgPoint, animated: true)
+        }
+        
 
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -48,4 +59,9 @@ extension ViewController: UIScrollViewDelegate {
         }
         return nil
     }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(scrollView.contentOffset)
+    }
+//    func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
+//    }
 }
